ff4bd98778dbf1c5fdacfc11caf0146a8a157ff6
Evan Song <evace93@gmail.com>
2020-09-22 11:22:08 +0900

    fix: Auto Generate 이용하여 동작할 수 있도록 수정

--- app/src/main/java/com/ddd4/synesthesia/beer/presentation/ui/home/NavigationDirections.kt
+++ app/src/main/java/com/ddd4/synesthesia/beer/presentation/ui/home/NavigationDirections.kt
@@ -1,44 +1,14 @@
 package com.ddd4.synesthesia.beer.presentation.ui.home
 
-import android.os.Bundle
-import android.os.Parcelable
 import androidx.navigation.ActionOnlyNavDirections
 import androidx.navigation.NavDirections
 import com.ddd4.synesthesia.beer.R
 import com.ddd4.synesthesia.beer.data.model.Beer
-import java.io.Serializable
 
 class NavigationDirections private constructor() {
 
-    @Suppress("CAST_NEVER_SUCCEEDS")
-    private data class ActionToDetail(
-        val beer: Beer
-    ) : NavDirections {
-
-        override fun getArguments(): Bundle {
-            val result = Bundle()
-            when {
-                Parcelable::class.java.isAssignableFrom(Beer::class.java) -> result.putParcelable(
-                    "Beer",
-                    this.beer as Parcelable
-                )
-                Serializable::class.java.isAssignableFrom(Beer::class.java) -> result.putSerializable(
-                    "Beer",
-                    this.beer as Serializable
-                )
-                else -> throw UnsupportedOperationException(Beer::class.java.name)
-            }
-            return result
-        }
-
-        override fun getActionId() = R.id.action_to_detail
-
-    }
-
-
     companion object {
-        fun actionToDetail(beer: Beer): NavDirections = ActionToDetail(beer)
-
+        fun actionToDetail(beer: Beer): NavDirections = ActionOnlyNavDirections(R.id.action_to_detail)
         fun actionToMyPage(): NavDirections = ActionOnlyNavDirections(R.id.action_to_mypage)
     }
 }
